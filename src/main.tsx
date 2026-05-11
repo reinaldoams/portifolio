79c06e79bbd863479f673efec924c1e523f51a25
Reinaldo Assis <reinaldoams@gmail.com>
2026-05-11 15:05:29 -0300

    feat: adding vercel analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './main.scss'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
+      <Analytics />
       <App />
     </BrowserRouter>
   </StrictMode>,
